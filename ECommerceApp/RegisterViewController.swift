6e12fe3c79e86d970438f4dcd04b319955625e81
n0an <nantog@gmail.com>
2017-11-14 13:41:27 +0300

    Registration UI created

---
+++ ECommerceApp/RegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterViewController.swift
+//  ECommerceApp
+//
+//  Created by nag on 14/11/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
--- ECommerceApp/ViewController.swift
+++ ECommerceApp/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         
     }
 
-
+    
 
 }
 
