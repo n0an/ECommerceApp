0e5196ee57b8a2622fca4c74492e369ad50f7fbd
n0an <nantog@gmail.com>
2017-11-16 11:56:32 +0300

    AddPropertyVC outlets and actions connected

--- ECommerceApp/AddPropertyViewController.swift
+++ ECommerceApp/AddPropertyViewController.swift
@@ -10,26 +10,70 @@ import UIKit
 
 class AddPropertyViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var referenceCodeTextField: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var roomsTextField: UITextField!
+    @IBOutlet weak var bathroomsTextField: UITextField!
+    @IBOutlet weak var propertySizeTextField: UITextField!
+    @IBOutlet weak var balconySizeTextField: UITextField!
+    @IBOutlet weak var parkingTextField: UITextField!
+    @IBOutlet weak var floorTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var advertisementTypeTextField: UITextField!
+    @IBOutlet weak var availableFromTextField: UITextField!
+    @IBOutlet weak var buildYearTextField: UITextField!
+    @IBOutlet weak var propertyTypeTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBOutlet weak var titleDeedSwitch: UISwitch!
+    @IBOutlet weak var centralHeatingSwitch: UISwitch!
+    @IBOutlet weak var solarWaterHeatingSwitch: UISwitch!
+    @IBOutlet weak var storeRoomSwitch: UISwitch!
+    @IBOutlet weak var airConditionerSwitch: UISwitch!
+    @IBOutlet weak var furnishedSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        scrollView.contentSize = CGSize(width: self.view.bounds.width, height: topView.frame.height)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - ACTIONS
+    
+    @IBAction func actionSwitchTriggered(_ sender: Any) {
     }
-    */
+    
+    
+    
+    @IBAction func actionPinLocationButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func actionCurrentLocationButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func actionCameraButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func actionSaveButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    
 
 }
