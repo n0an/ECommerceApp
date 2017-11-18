bca10773bbf55a6181c3755d100abb92d9bd871b
n0an <nantog@gmail.com>
2017-11-18 22:20:34 +0300

    Downloader: convertImagesToDataArray bug fixed

--- ECommerceApp/PropertyVC/MapViewCell.swift
+++ ECommerceApp/PropertyVC/MapViewCell.swift
@@ -17,24 +17,20 @@ class MapViewCell: UITableViewCell {
     
     func configureCell(property: Property) {
         
-        var region = MKCoordinateRegion()
-        
         guard property.latitude != 0.0 && property.longitude != 0.0 else {
             return
         }
         
-        region.center.latitude = property.latitude
-        region.center.longitude = property.longitude
+        let propertyLocation = CLLocationCoordinate2D(latitude: property.latitude, longitude: property.longitude)
         
-        region.span.latitudeDelta = 10
-        region.span.longitudeDelta = 10
+        let region = MKCoordinateRegionMakeWithDistance(propertyLocation, 1000, 1000)
         
         mapView.setRegion(region, animated: true)
         
         let annotation = MKPointAnnotation()
         annotation.title = property.title
         annotation.subtitle = "\(property.numberOfRooms) bedroom \(property.properyType!)"
-        annotation.coordinate = CLLocationCoordinate2D(latitude: property.latitude, longitude: property.longitude)
+        annotation.coordinate = propertyLocation
         
         mapView.addAnnotation(annotation)
         
