58516abab19791790a7703802ec2e064e15a5308
n0an <nantog@gmail.com>
2017-11-19 11:25:54 +0300

    FavoriteVC UI created

---
+++ ECommerceApp/FavoriteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteViewController.swift
+//  ECommerceApp
+//
+//  Created by Anton Novoselov on 19/11/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+}
