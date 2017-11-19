c2852e423326442e330b540c765a4b0138226641
n0an <nantog@gmail.com>
2017-11-19 12:34:20 +0300

    MyPropertiesVC UI created

---
+++ ECommerceApp/MyPropertiesViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MyPropertiesViewController.swift
+//  ECommerceApp
+//
+//  Created by Anton Novoselov on 19/11/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class MyPropertiesViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+
+}
